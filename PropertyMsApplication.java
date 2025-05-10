3bd420524262749e466060c891bccddeb6635b68
VEERLAPATIABHIRAM <163742407+VEERLAPATIABHIRAM@users.noreply.github.com>
2025-05-11 01:42:27 +0530

    PropertyMsApplication.java

---
+++ PropertyMsApplication.java
@@ -0,0 +1,13 @@
+package com.klu.PropertyMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyMsApplication.class, args);
+	}
+
+}
